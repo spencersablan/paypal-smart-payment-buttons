d4968e438fc2169ef84e432bf942d3131a43930c
Spencer Sablan <spencertsablan@gmail.com>
2023-02-17 09:18:22 -0700

    fixes for ci

--- src/payment-flows/native/eligibility.js
+++ src/payment-flows/native/eligibility.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import type { ZalgoPromise } from '@krakenjs/zalgo-promise/src';
-import { ENV, FUNDING, PLATFORM } from '@paypal/sdk-constants/src';
+import { FUNDING, PLATFORM } from '@paypal/sdk-constants/src';
 import { supportsPopups, isIos, isAndroid } from '@krakenjs/belter/src';
 import type { CrossDomainWindowType } from '@krakenjs/cross-domain-utils/src';
 import type { ProxyWindow } from '@krakenjs/post-robot/src';
@@ -134,7 +134,7 @@ export function canUseNativeQRCode({ fundingSource, win } : {| fundingSource : ?
 }
 
 export function isNativeEligible({ props, config, serviceData } : IsEligibleOptions) : boolean {
-    const { fundingSource, onShippingChange, createBillingAgreement, createSubscription, env, platform } = props;
+    const { fundingSource, onShippingChange, createBillingAgreement, createSubscription, platform } = props;
     const { firebase: firebaseConfig } = config;
     const { cookies, merchantID, fundingEligibility } = serviceData;
     const isVenmoEligible = fundingEligibility?.venmo?.eligible;
